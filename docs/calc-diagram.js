7fe1fabfa4edcabfe915737b234c9d23504455de
David Megginson <david.megginson@gmail.com>
2024-03-08 11:58:35 -0500

    Converted coordinates from 100x100 to 1000x1000.

--- docs/calc-diagram.js
+++ docs/calc-diagram.js
@@ -20,11 +20,11 @@ window.onload = () => {
 
         // figure out where to place the tick, on a circular log10 scale
         let rotation = Math.log10(i) * 360.0;
-        let transform  = "rotate(" + rotation + ", 50, 50)";
+        let transform  = "rotate(" + rotation + ", 500, 500)";
 
         // tick defaults (small tick)
-        let tick_offset = 2;
-        let tick_stroke = 0.1;
+        let tick_offset = 20;
+        let tick_stroke = 1;
 
         if (i > 150 && i < 300 && (i % 2) != 0) {
             continue;
@@ -38,35 +38,35 @@ window.onload = () => {
 
         // larger ticks
         if (i < 10 || (i < 150 && (i % 5) == 0) || (i < 300 && (i % 20) == 0) || (i < 600 && (i % 10) == 0)) {
-            tick_offset = 3;
-            tick_stroke = 0.2;
+            tick_offset = 30;
+            tick_stroke = 2;
         }
 
         outer_wheel_node.appendChild(makeElement("line", {
-            x1: 50, x2: 50, y1: (8 - tick_offset), y2: 8, stroke: "black", "stroke-width": tick_stroke, transform: transform
+            x1: 500, x2: 500, y1: (80 - tick_offset), y2: 80, stroke: "black", "stroke-width": tick_stroke, transform: transform
         }));
 
         inner_wheel_node.appendChild(makeElement("line", {
-            x1: 50, x2: 50, y1: 8, y2: 8 + tick_offset, stroke: "black", "stroke-width": tick_stroke, transform: transform
+            x1: 500, x2: 500, y1: 80, y2: 80 + tick_offset, stroke: "black", "stroke-width": tick_stroke, transform: transform
         }));
 
         // labels
         if (i < 20 || (i < 60 && (i % 5) == 0)) {
             let label_text = "" + (i <= 9 ? i * 10 : i);
-            if (i == 10 || i == 6) {
+            if (i == 10) {
                 continue; // already drawing this as a special circle
             }
             outer_wheel_node.appendChild(makeElement("text", {
-                x: 50, y: 4.5, class: "label", transform: transform
+                x: 500, y: 45, class: "label", transform: transform
             }, label_text));
 
             inner_wheel_node.appendChild(makeElement("text", {
-                x: 50, y: 13, class: "label", transform: transform
+                x: 500, y: 130, class: "label", transform: transform
             }, label_text));
         }
 
     }
 
     // rotate the outer wheel a bit
-    outer_wheel_node.setAttribute("transform", "rotate(" + Math.log10(1.51) * -360.0 + ", 50, 50)");
+    outer_wheel_node.setAttribute("transform", "rotate(" + Math.log10(1.51) * -360.0 + ", 500, 500)");
 };
